24ea8eeef4f8e3e25caf38a07af56dece4b7493c
Marco <m.rotondi@ngi.it>
2019-12-15 00:31:07 +0100

    Init project on intrè github

---
+++ src/main/java/it/intre/App.java
@@ -0,0 +1,13 @@
+package it.intre;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Test Container!" );
+    }
+}
---
+++ src/test/java/it/intre/TestContainerTest.java
@@ -0,0 +1,29 @@
+package it.intre;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+public class TestContainerTest {
+    private static final Logger log = LoggerFactory.getLogger(Testcontainers.class);
+
+    @Container
+    private PostgreSQLContainer postgresqlContainer = new PostgreSQLContainer()
+            .withDatabaseName("giftcard-test")
+            .withUsername("giftcard")
+            .withPassword("secret");
+
+    @Test
+    void test_container_up() {
+        Assertions.assertTrue(postgresqlContainer.isRunning());
+        Assertions.assertTrue(postgresqlContainer.getJdbcUrl().contains("giftcard-test"));
+
+        log.info("### JDBC URL {}", postgresqlContainer.getJdbcUrl());
+    }
+
+}
